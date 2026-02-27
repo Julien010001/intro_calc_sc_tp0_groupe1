8ab0478dec8beaac7e75b3c637ed1cfe2cd07395
Julien010001 <165374267+Julien010001@users.noreply.github.com>
2026-02-27 12:03:00 +0100

    ajout type de retour pour nombre_racines dans q3.py

--- q3.py
+++ q3.py
@@ -4,7 +4,7 @@ import numpy as np
 def f(a, x):
     return a * np.log((2*x)/(x+1)) - x/(2*x+1)
 
-def nombre_racines(a: float):
+def nombre_racines(a: float) -> int:
     """
     1/(2*np.log(2)) a un trop grand nombre de décimales,
     on pourrait remplacer notre condition par :
@@ -17,4 +17,5 @@ def nombre_racines(a: float):
 
 
 if __name__ == "__main__":
+
     print(abs(0.72134752 - 1/(2*np.log(2))) < 1e-8)
