7179bf6ba5f097bbf5f8d4a356ab578eb78b4492
Julien010001 <165374267+Julien010001@users.noreply.github.com>
2026-02-27 12:12:01 +0100

    Modification documentation
    Ajout de documentation pour la fonction nombre_racines dans q3.py

--- q3.py
+++ q3.py
@@ -5,11 +5,18 @@ def f(a, x):
     return a * np.log((2*x)/(x+1)) - x/(2*x+1)
 
 def nombre_racines(a: float) -> int:
+   """
+    Retourne le nombre de racines de f_a en fonction du a passé en paramètre
+
+    Args:
+        float: a
+
+    Return:
+        int: le nombre de racine de f_a
     """
-    1/(2*np.log(2)) a un trop grand nombre de décimales,
-    on pourrait remplacer notre condition par :
-    abs(a - 1/(2*np.log(2))) < 1e-8
-    """
+    # 1/(2*np.log(2)) a un grand nombre de décimales
+    # on pourrait accepter une faible erreur et remplacer notre condition par :
+    # abs(a - 1/(2*np.log(2))) < 1e-8
     if a == 1/(2*np.log(2)):
         return 0
     else:
@@ -19,3 +26,4 @@ def nombre_racines(a: float) -> int:
 if __name__ == "__main__":
 
     print(abs(0.72134752 - 1/(2*np.log(2))) < 1e-8)
+
