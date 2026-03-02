89684e07d6fe07efa7efd99b7c6e041a7f3699cd
Julien010001 <165374267+Julien010001@users.noreply.github.com>
2026-03-02 17:10:32 +0100

    Modif q3 problème indentation

--- q3.py
+++ q3.py
@@ -4,12 +4,11 @@ import numpy as np
 def f(a, x):
     """
     Retourne f_a(x)
-    (a été utilisé pour visualiser la fonction avec matplotlib)
     """
     return a * np.log((2*x)/(x+1)) - x/(2*x+1)
 
 def nombre_racines(a: float) -> int:
-   """
+    """
     Retourne le nombre de racines de f_a en fonction du a passé en paramètre
 
     Args:
@@ -18,9 +17,6 @@ def nombre_racines(a: float) -> int:
     Return:
         int: le nombre de racine de f_a
     """
-    # 1/(2*np.log(2)) a un grand nombre de décimales
-    # on pourrait accepter une faible erreur et remplacer notre condition par :
-    # abs(a - 1/(2*np.log(2))) < 1e-8
     if a == 1/(2*np.log(2)):
         return 0
     else:
@@ -44,3 +40,4 @@ if __name__ == "__main__":
     plt.legend()
     plt.grid(True)
     plt.show()
+
